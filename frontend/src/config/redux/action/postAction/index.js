dfb942ff4990817fa78ba26ca1ade72b90a4ea2e
seema-1802 <seemathakor257@gmail.com>
2026-06-15 17:39:54 +0530

    Alert  initial commit

--- frontend/src/config/redux/action/postAction/index.js
+++ frontend/src/config/redux/action/postAction/index.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { clientServer } from "@/config";
-
+import Swal from "sweetalert2";
 // Create a post
 export const createPostAction = createAsyncThunk(
   "post/createPost",
@@ -8,8 +8,20 @@ export const createPostAction = createAsyncThunk(
     try {
       const response = await clientServer.post("/post/createPost", postData); 
       // default axios content-type is application/json
+        Swal.fire({
+        icon: "success",
+        title: "Success",
+        text: "Post created successfully!",
+        timer: 2000,
+        showConfirmButton: false,
+      });
       return response.data.post;
     } catch (error) {
+       Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: error.response?.data?.error || "Create post failed",
+      });
       return thunkAPI.rejectWithValue(
         error.response?.data?.error || "Create post failed"
       );
@@ -36,9 +48,21 @@ export const deletePostAction = createAsyncThunk(
   "post/deletePost",
   async ({ postId, userId }, thunkAPI) => {
     try {
-      const response = await clientServer.post("/post/deletePost", { postId, userId }); // ✅ add /post
+      const response = await clientServer.post("/post/deletePost", { postId, userId });
+        Swal.fire({
+        icon: "success",
+        title: "Deleted",
+        text: "Post deleted successfully!",
+        timer: 2000,
+        showConfirmButton: false,
+      });
       return response.data;
     } catch (error) {
+       Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: error.response?.data?.error || "Delete post failed",
+      });
       return thunkAPI.rejectWithValue(
         error.response?.data?.error || "Delete post failed"
       );
@@ -76,8 +100,20 @@ export const createCommentAction = createAsyncThunk(
         userId,
         body,
       });
+        Swal.fire({
+        icon: "success",
+        title: "Success",
+        text: "Comment added successfully!",
+        timer: 1500,
+        showConfirmButton: false,
+      });
       return response.data.comment;
     } catch (error) {
+       Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: error.response?.data?.error || "Create comment failed",
+      });
       return thunkAPI.rejectWithValue(
         error.response?.data?.error || "Create comment failed"
       );
