f84d8ce9751d32a4723de5e330a6f0460034f3bd
seema-1802 <seemathakor257@gmail.com>
2026-06-28 13:10:41 +0530

    update forgot-password   initial commit

---
+++ frontend/src/pages/forgot-password/index.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import Swal from "sweetalert2";
+  import { useDispatch } from "react-redux";
+import { forgotPassword } from "@/redux/authThunk";
+import { useRouter } from "next/router";
+
+export default function ForgotPassword() {
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+
+const dispatch = useDispatch();
+
+const router = useRouter();
+
+const handleSubmit = async () => {
+  if (!email) {
+    Swal.fire("Error", "Please enter email", "error");
+    return;
+  }
+
+  try {
+    setLoading(true);
+
+    const res = await dispatch(forgotPassword(email)).unwrap();
+
+    Swal.fire({
+      icon: "success",
+      title: "Success",
+      text: res.message,
+    });
+
+    setTimeout(() => {
+     router.push(`/reset-password/${res.token}`);
+    }, 1500);
+
+  } catch (err) {
+    Swal.fire("Error", err, "error");
+  } finally {
+    setLoading(false);
+  }
+ };
+
+  return (
+    <div style={{ padding: 50 }}>
+      <h2>Forgot Password</h2>
+
+      <input
+        type="email"
+        placeholder="Enter Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <button onClick={handleSubmit}>
+        {loading ? "Sending..." : "Send Reset Link"}
+      </button>
+    </div>
+  );
+}
